33c546fdceddbdce7782b1a25f43dd8fefb315ba
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-04-03 16:11:02 +0600

    styled the card box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Suspense } from 'react';
 import './App.css'
 import WeatherCard from './components/WeatherCard/WeatherCard'
+
+const weatherData = fetch('./weatherData.json')
+.then(res => res.json());
 
 function App() {
 
   return (
     <>
-      <WeatherCard></WeatherCard>
+      
+      <Suspense fallback={<h4>loading....</h4>}>
+        <WeatherCard weatherData = {weatherData}></WeatherCard>
+      </Suspense>
     </>
   )
 }
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { use } from 'react';
 import './WeatherCard.css'
 
-const WeatherCard = () => {
+const WeatherCard = ({weatherData}) => {
+
+    const Weather = use(weatherData)
+    
+    console.log(Weather);
+
+    const {name} = Weather;
+
     return (
         <div className='card'>
-            <h1>hello</h1>
+            <h1>{name}, {Weather.sys.country}</h1>
+            {/* <img src={Weather.weather[0].icon} alt="" /> */}
+            
+            <p>{Weather.weather[0].description}</p>
         </div>
     );
 };
