e13d9dc6f3839cef7b780b013c8ec4551c108391
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-04-03 15:36:19 +0600

    added weather data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WeatherCard from './components/WeatherCard/WeatherCard'
+
+function App() {
+
+  return (
+    <>
+      <WeatherCard></WeatherCard>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeatherCard.css'
+
+const WeatherCard = () => {
+    return (
+        <div className='card'>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default WeatherCard;
