17f8eee407331d2200245920ad359efb3a54349a
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-04-03 19:08:43 +0600

    adding data in weather box

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,18 +3,38 @@ import './WeatherCard.css'
 
 const WeatherCard = ({weatherData}) => {
 
+    function findDirection(degree){
+        const directions = [
+            "North", "North-Northeast", "Northeast", "East-Northeast", "East",
+        "East-Southeast", "Southeast", "South-Southeast", "South", 
+        "South-Southwest", "Southwest", "West-Southwest", "West", 
+        "West-Northwest", "Northwest", "North-Northwest"
+        ];
+
+        const index = Math.round(degree / 22.5) % 16;
+        return directions[index];
+    }
+
     const Weather = use(weatherData)
     
     console.log(Weather);
 
     const {name} = Weather;
+    const degree = Weather.wind.deg;
+    
 
     return (
         <div className='card'>
             <h1>{name}, {Weather.sys.country}</h1>
+
             {/* <img src={Weather.weather[0].icon} alt="" /> */}
             
-            <p>{Weather.weather[0].description}</p>
+            <h1 className='temperature'>{Weather.weather[0].description}</h1>
+            <h1 className='temperature'>{Weather.main.temp}°C</h1>
+
+            <p>Wind: {Weather.wind.speed}, {findDirection(degree)}</p>
+            <p>Rain (): {Weather.rain["1h"]}mm</p>
+
         </div>
     );
 };
